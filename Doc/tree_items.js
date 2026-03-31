64adb6544b3a1a8b59f0a6bb96a27e4c561b1ee6
Kyohei Yazawa <kyazawa@umn.edu>
2026-03-31 09:50:51 -0500

    Add artifacts

---
+++ Doc/tree_items.js
@@ -0,0 +1,12 @@
+var TREE_ITEMS = [
+["", 0, 0,
+
+  ["Household", 0, 0,
+
+    ["Technical", 0, 0,
+      ["rectype - Record type", "javascript:top.tf.tnode('rectype')"],
+      ["year - Census year", "javascript:top.tf.tnode('year')"],
+    ],
+  ],
+]
+];
